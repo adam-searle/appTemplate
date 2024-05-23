39b579cba0935e161e9ae353a23ecf5c59f04983
adamSearle <adam-searle@hotmail.com>
2024-05-23 07:46:19 +0100

    Update firebaseConfig.ts
    HIDE SECRETS

--- constants/firebaseConfig.ts
+++ constants/firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBNaz9-n4uBY06ts6K-x5TOCMXvEZN7d_0",
-  authDomain: "calmfocus-3321a.firebaseapp.com",
-  projectId: "calmfocus-3321a",
-  storageBucket: "calmfocus-3321a.appspot.com",
-  messagingSenderId: "300398017560",
-  appId: "1:300398017560:web:051159852bad269fe0976d",
-  measurementId: "G-15SYWL3M2Q",
+  apiKey: "-",
+  authDomain: "-",
+  projectId: "-",
+  storageBucket: "-",
+  messagingSenderId: "-",
+  appId: "-",
+  measurementId: "-",
 };
 
 let app;
